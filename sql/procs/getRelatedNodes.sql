242b442dd68ff7937d3157381abff70d9f444774
drkameleon <yaniszaf@gmail.com>
2022-01-24 17:21:12 +0100

    also get the edge id

--- sql/procs/getRelatedNodes.sql
+++ sql/procs/getRelatedNodes.sql
@@ -1,4 +1,4 @@
-SELECT nodes.id, nodes.tag, nodes.properties, source, target, edges.tag AS label
+SELECT nodes.id, nodes.tag, nodes.properties, source, target, edges.tag, edges.id AS label
 FROM edges 
 INNER JOIN nodes
 ON nodes.id=edges.source OR nodes.id=edges.target
